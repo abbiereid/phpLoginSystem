a1e8d71313948ed422375a36d7c1e8981dec6176
abbie <abbiereid222@gmail.com>
2024-03-19 21:52:08 +0000

    .

--- login.php
+++ login.php
@@ -4,7 +4,8 @@
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-      }
-    echo "Connected successfully";
+    }
+    
+    echo $_POST['username'];
     
 ?>
