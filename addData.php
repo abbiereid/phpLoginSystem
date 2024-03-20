15bbccb928ac19088dfe40582a4356273f96e3be
abbie <abbiereid222@gmail.com>
2024-03-20 10:49:38 +0000

    .

--- addData.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $conn = new mysqli("localhost", "root", "02122003", "users");
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-?>
--- dataAccess.php
+++ dataAccess.php
@@ -6,4 +6,25 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
+    $username = $_POST['username'];
+
+    $sql = "SELECT * FROM `userlinks` WHERE `user` = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $response = array();
+
+    if ($result->num_rows > 0) {
+
+        
+
+        $response['name'] = $name;
+        $response['link'] = $link;
+
+
+
+    }
+
 ?>
