1ef62f0069ea3354ee24721a117f5ad16a51b494
abbie <abbiereid222@gmail.com>
2024-03-20 09:53:23 +0000

    getting php to return JSON instead, so i can stick to spa

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn = new mysqli("localhost", "root", "02122003","users");
+    $conn = new mysqli("localhost", "root", "02122003", "users");
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -9,29 +9,31 @@
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+
     $sql = "SELECT * FROM `userdetails` WHERE `username` = ? AND `password` = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ss", $username, $password);
-
     $stmt->execute();
     $result = $stmt->get_result();
 
-    if (!($result->num_rows > 0)) {
-        echo "
-            <div class='popup'>
-                <h2>This user does not exist, or the password you have entered is incorrect</h2>
-                <button class='back'>Back</button>
-            </div>
-        ";
+    $response = array();
+
+    if ($result->num_rows > 0) {
+        $userRow = $result->fetch_assoc();
+        $dob = $userRow['DOB'];
+
+        $response['success'] = true;
+        $response['username'] = $username;
+        $response['dob'] = $dob;
     } else {
-        $userRow = $result->fetch_assoc(); // Fetch user details
-        $dob = $userRow['DOB']; // Fetch DOB from the user details
-        echo "
-            Welcome $username, I see it is your birthday on the $dob
-        ";
+        $response['success'] = false;
+        $response['message'] = "This user does not exist, or the password you have entered is incorrect";
     }
 
     $stmt->close();
     $conn->close();
 
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
 ?>
